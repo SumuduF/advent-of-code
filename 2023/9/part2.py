7da623e520a2eace70c70673921d51cfd66c5c55
Sumudu Fernando <sumuduf@gmail.com>
2023-12-08 21:07:51 -0800

    Initial solutions for day 9
    Well that was easy! Got on the global board for once (to get higher
    ranks I'd probably need to work on downloading the input faster).
    
    I was all ready to pull out the fancy integer polynomial fitting formula
    (i.e. calculate the value many steps out) but part2 was just a minor
    change.  I did it by switching + to - (as I plan to merge the two parts
    later), but part2 can also be solved by just reversing each input to
    part1 solution.
    
    Also note there's no need to go all the way down to 0's: can terminate
    once we hit all values equal which is just as easy to detect.

---
+++ 2023/9/part2.py
@@ -0,0 +1,32 @@
+import argparse
+import sys
+
+def main(filename):
+    lines = read_file(filename)
+    task = parse_task(lines)
+    answer = solve(task)
+
+    print(answer)
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        lines = [line.strip() for line in f]
+    return lines
+
+def parse_task(lines):
+    return [list(map(int, line.split())) for line in lines]
+
+def solve(task):
+    return sum(prev_val(hist) for hist in task)
+
+def prev_val(hist):
+    if all(v == 0 for v in hist): return 0
+    n = len(hist)
+    diffs = [hist[i] - hist[i-1] for i in range(1, n)]
+    return hist[0] - prev_val(diffs)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", nargs="?", default="input.txt")
+    args = parser.parse_args()
+    sys.exit(main(args.filename))
