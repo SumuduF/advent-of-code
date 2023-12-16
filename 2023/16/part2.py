eedec4a96ada1da0fd7e993015a01ed65a3dc7e0
Sumudu Fernando <sumuduf@gmail.com>
2023-12-15 21:21:15 -0800

    Initial solutions for day 16
    Pretty much just do it (BFS).
    
    I could have written simpler code for the propagation if I had just
    focused on the direction part (no matter what, the other part of the
    state is just obtained by adding the new direction to current pos,
    obviously).
    
    Part 2 is a straightforward expansion where we just try shooting the
    beam in from every possible spot so nothing complicated there.

---
+++ 2023/16/part2.py
@@ -0,0 +1,78 @@
+import argparse
+import sys
+
+def main(filename):
+    lines = read_file(filename)
+    task = parse_task(lines)
+    answer = solve(task)
+
+    print(answer)
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        lines = [line.strip() for line in f]
+    return lines
+
+def parse_task(lines):
+    return [list(line) for line in lines]
+
+def solve(task):
+    n, m = len(task), len(task[0])
+
+    best = 0
+    for sr in range(n):
+        best = max(best, count_energized(task, sr, 0, 0, 1))
+        best = max(best, count_energized(task, sr, m-1, 0, -1))
+    for sc in range(m):
+        best = max(best, count_energized(task, 0, sc, 1, 0))
+        best = max(best, count_energized(task, n-1, sc, -1, 0))
+    return best
+
+def count_energized(task, sr, sc, sdr, sdc):
+    n, m = len(task), len(task[0])
+    # state is row, col, dr, dc
+    seen = set()
+    q = list()
+    def push(row, col, dr, dc):
+        if not 0 <= row < n: return
+        if not 0 <= col < m: return
+
+        state = (row, col, dr, dc)
+        if state not in seen:
+            q.append(state)
+            seen.add(state)
+
+    push(sr, sc, sdr, sdc)
+    while q:
+        pq = q
+        q = []
+        for (row, col, dr, dc) in pq:
+            for (nrow, ncol, ndr, ndc) in propagate(task, row, col, dr, dc):
+                push(nrow, ncol, ndr, ndc)
+
+    return len(set((row, col) for (row, col, _, _) in seen))
+
+def propagate(task, r, c, dr, dc):
+    if task[r][c] == ".": yield (r + dr, c + dc, dr, dc)
+    elif task[r][c] == "/":
+        dr, dc = -dc, -dr
+        yield (r + dr, c + dc, dr, dc)
+    elif task[r][c] == "\\":
+        dr, dc = dc, dr
+        yield (r + dr, c + dc, dr, dc)
+    elif dr == 0: # horizontal
+        if task[r][c] == "-": yield (r + dr, c + dc, dr, dc)
+        else:
+            yield (r + 1, c, 1, 0)
+            yield (r - 1, c, -1, 0)
+    else: # vertical
+        if task[r][c] == "|": yield (r + dr, c + dc, dr, dc)
+        else:
+            yield (r, c + 1, 0, 1)
+            yield (r, c - 1, 0, -1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", nargs="?", default="input.txt")
+    args = parser.parse_args()
+    sys.exit(main(args.filename))
