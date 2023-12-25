a3985cffe486ac29e1f5ef97c10e58ba866ca38a
Sumudu Fernando <sumuduf@gmail.com>
2023-12-24 23:46:09 -0600

    Solution for Day 25
    Hammer it using min-cut-max-flow.
    
    I didn't realize there wouldn't be a part 2, so I kind of took it slow
    thinking that I'd probably spend a lot more time on part 2 anyway.
    
    Heavily followed an old flow solution I had lying around (but that was
    slightly different because it was C++ and also had directed edges with
    varying capacity: this flow is simpler because the edges are
    bidirectional and the flow is 0-1.
    
    Since we don't know what the cut separates, try various sinks until we
    get a cut of 3. Stoer-Wagner algorithm could be used to do something
    smarter (but that is so much extra work here...)
    
    There's also probabilistic approach to global min-cut: Karger's
    algorithm. Since we even know the cut-size = 3 we can probably do even
    better than usual with this.

---
+++ 2023/25/part1.py
@@ -0,0 +1,93 @@
+import argparse
+import sys
+from collections import defaultdict
+from collections import deque
+
+def main(filename):
+    lines = read_file(filename)
+    task = parse_task(lines)
+    answer = solve(task)
+
+    print(answer)
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        lines = [line.strip() for line in f]
+    return lines
+
+def parse_task(lines):
+    edges = []
+    for line in lines:
+        a, _, bs = line.partition(": ")
+        for b in bs.split():
+            edges.append((a, b))
+    return edges
+
+def solve(task):
+    flows = defaultdict(dict)
+    for (u, v) in task:
+        flows[u][v] = 0
+        flows[v][u] = 0
+
+    nodes = set(flows.keys())
+    source = nodes.pop()
+    for sink in nodes:
+        flow_val = maxflow(flows, source, sink)
+        if flow_val == 3:
+            break
+        for u in flows:
+            for v in flows[u]:
+                flows[u][v] = 0
+
+    side = set()
+    q = deque([source])
+    while q:
+        u = q.popleft()
+        side.add(u)
+        for v in flows[u]:
+            if v not in side and (flows[u][v] < 1):
+                q.append(v)
+                side.add(v)
+
+    a = len(side)
+    b = len(flows.keys()) - a
+
+    return a * b
+
+def maxflow(flows, source, sink):
+    ans = 0
+    while True:
+        delta = augment(flows, source, sink)
+        if not delta: break
+        ans += delta
+    return ans
+
+def augment(flows, source, sink):
+    prev = dict()
+    aug = defaultdict(int)
+    aug[source] = 1E9
+
+    q = deque([source])
+    while q and not aug[sink]:
+        u = q.popleft()
+        for v in flows[u]:
+            if not aug[v]:
+                if flows[u][v] < 1:
+                    prev[v] = u
+                    aug[v] = min(aug[u], 1 - flows[u][v])
+                    q.append(v)
+
+    if aug[sink] > 0:
+        cur = sink
+        while cur != source:
+            pre = prev[cur]
+            flows[pre][cur] += aug[sink]
+            flows[cur][pre] -= aug[sink]
+            cur = pre
+    return aug[sink]
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", nargs="?", default="input.txt")
+    args = parser.parse_args()
+    sys.exit(main(args.filename))
