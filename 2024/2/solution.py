0946b90de3f9330bee416192ea2947f838f21a28
Sumudu Fernando <sumuduf@gmail.com>
2024-12-01 21:14:21 -0800

    Solution for day 2
    Quite fast (but there were issues with the site this time!)
    
    zip(lst, tail) is a handy idiom for this kind of thing.
    
    For part 2 just did the most naive approach; in theory one could try to
    be cleverer but the reports were very short so no reason to do that.

---
+++ 2024/2/solution.py
@@ -0,0 +1,20 @@
+from aoclib import util
+
+def solution(input_lines):
+    reports = [[int(v) for v in line.split()] for line in input_lines]
+
+    part1_ans = sum(is_safe(report) for report in reports)
+    part2_ans = sum(is_tolerable(report) for report in reports)
+
+    return (part1_ans, part2_ans)
+
+def is_safe(report):
+    dels = [ b-a for (a, b) in zip(report, report[1:])]
+    return all(1 <= d <= 3 for d in dels) or all(-3 <= d <= -1 for d in dels)
+
+def is_tolerable(report):
+    if is_safe(report): return True
+    n = len(report)
+    return any(is_safe(report[:i] + report[(i+1):]) for i in range(n))
+
+if __name__ == "__main__": util.run_solution(solution)
