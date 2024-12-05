514d6fc108ca7962e8614d7f96b2230b4f16b869
Sumudu Fernando <sumuduf@gmail.com>
2024-12-04 21:49:33 -0800

    [2024] Solution for day 5
    Originally I just used bubblesort, but built-in sort can be used too.
    
    The input is described as a "very specific order" which actually means
    there is an entry in the rules for every possible pair. In other words
    we don't need to the transitive closure of the relationships (which
    bubblesort would do implicitly, if written in an appropriately tentative
    way).
    
    Anyway the outline of the solution is simple: sort each list based on
    the ordering and add the middle number to the right total depending on
    if it changed or not.

---
+++ 2024/5/solution.py
@@ -0,0 +1,29 @@
+from aoclib import util
+from functools import cmp_to_key
+
+def solution(input_lines):
+    orders, updates = util.line_groups(input_lines)
+
+    # "very specific order" means all pairs are actually there
+    # raise an error in case we ever try to compare incomparable values
+    pairs = set(tuple(map(int, line.split("|"))) for line in orders)
+    def cmp(p, q):
+        if (p, q) in pairs: return -1
+        if (q, p) in pairs: return 1
+        raise ValueError(f"no pair for {p} and {q}")
+    by_pairs = cmp_to_key(cmp)
+
+    part1_ans, part2_ans = 0, 0
+    for line in updates:
+        pages = list(map(int, line.split(",")))
+        fixed = sorted(pages, key=by_pairs)
+
+        val = fixed[len(fixed) // 2]
+        if pages == fixed:
+            part1_ans += val
+        else:
+            part2_ans += val
+
+    return (part1_ans, part2_ans)
+
+if __name__ == "__main__": util.run_solution(solution)
