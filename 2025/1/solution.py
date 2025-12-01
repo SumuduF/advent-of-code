128f72ee54e06c445bc1885f7855431d34562f88
Sumudu Fernando <sumuduf@gmail.com>
2025-11-30 21:31:53 -0800

    [2025] Solution for day 1
    Simple task (noting you just track position mod 100)
    
    Though for part2 a much simpler approach is to simply replace all the
    numbers by 1-step turns and reuse the counting from part1. Here I
    instead split it into full turns and partial turns and did cases.
    
    Parsing was too simple to bother using the library.

---
+++ 2025/1/solution.py
@@ -0,0 +1,26 @@
+from aoclib import util
+
+def solution(lines):
+    part1_ans = 0
+    part2_ans = 0
+
+    pos = 50
+    for line in lines:
+        num = int(line[1:])
+
+        full, rest = divmod(num, 100)
+        part2_ans += full
+
+        match line[0]:
+            case "L":
+                if 0 < pos <= rest: part2_ans += 1
+                pos = (pos - rest) % 100
+            case "R":
+                if 100-rest <= pos: part2_ans += 1
+                pos = (pos + rest) % 100
+
+        if pos == 0: part1_ans += 1
+
+    return (part1_ans, part2_ans)
+
+if __name__ == "__main__": util.run_solution(solution)
