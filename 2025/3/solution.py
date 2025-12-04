940404183f649e1b63acfe5d4a79aede63097833
Sumudu Fernando <sumuduf@gmail.com>
2025-12-03 21:56:01 -0800

    [2025] Update solution for day 3
    There's a much simpler approach here than the original DP.
    
    We can greedily choose digits from left to right, constrained only by
    that digits near the end of the bank cannot be placed too early in the
    number. This is modeled well using a stack where incoming entries simply
    kill off previous values that they render obsolete: this general pattern
    is worth remembering! Note also that the "active" part of the stack is
    thus always in non-decreasing order: I recall encountering this kind of
    state in some geometric sweep algorithms before.

--- 2025/3/solution.py
+++ 2025/3/solution.py
@@ -1,23 +1,23 @@
 from aoclib import util
 
 def solution(lines):
-    part1_ans = 0
-    part2_ans = 0
-
-    for line in lines:
-        bank = [int(c) for c in line]
-        n = len(bank)
-
-        # jolt[k][i] = jolt value taking k digits from first i digits of bank
-        # base cases are jolt[0][*] = 0, jolt[k][i<k] = 0
-        jolt = [[0] * (n+1) for _ in range(12+1)]
-        for k in range(1, 12+1):
-            for i in range(k, n+1):
-                jolt[k][i] = max(jolt[k][i-1], 10*jolt[k-1][i-1] + bank[i-1])
-
-        part1_ans += jolt[2][n]
-        part2_ans += jolt[12][n]
-
+    part1_ans = sum(joltage(line, 2) for line in lines)
+    part2_ans = sum(joltage(line, 12) for line in lines)
     return (part1_ans, part2_ans)
+
+def joltage(bank, m):
+    # track digits to keep in a stack; a higher digit is always better until it
+    # would leave too few digits in total
+    digits = []
+
+    n = len(bank)
+    for (i, c) in enumerate(bank):
+        lim = max(0, m - n + i)
+        while (len(digits) > lim) and (digits[-1] < c):
+            digits.pop()
+        if len(digits) < m:
+            digits.append(c)
+
+    return int("".join(digits))
 
 if __name__ == "__main__": util.run_solution(solution)
