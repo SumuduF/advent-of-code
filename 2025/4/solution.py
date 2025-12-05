75034329c30edd9c2d7dc837bdcd70135b0e8891
Sumudu Fernando <sumuduf@gmail.com>
2025-12-04 20:50:40 -0800

    [2025] Improved solution for day 4
    Simple tweak to only look at neighbors of last-peeled layer when
    computing the next one...since we already identify all those positions
    when evaluating the "peelability" might as well reuse the information.
    
    Structuring this as "layers" is a bit unnatural (just doing one removal
    at a time would be a bit simpler code-wise) but I'm doing it this way
    due to what part1 is asking.
    
    I couldn't come up with a substantially different approach for this
    problem, and also suspect that the best way to further speed this up
    would be to use a more performant array (e.g. something from numpy).

--- 2025/4/solution.py
+++ 2025/4/solution.py
@@ -4,15 +4,28 @@ from aoclib import grids
 def solution(lines):
     layers = []
     locs = set(loc for (loc, val) in grids.gnumerate(lines) if val == "@")
-    while freed := list(free(locs)):
-        layers.append(len(freed))
-        locs.difference_update(freed)
+    possibles = locs
+
+    while True:
+        peeled, possibles = peel_layer(locs, possibles)
+        if not peeled: break
+
+        layers.append(len(peeled))
+        locs.difference_update(peeled)
 
     return (layers[0], sum(layers))
 
-def free(locs):
-    for loc in locs:
-        around = sum(z in locs for z in grids.neighbors8(loc))
-        if around < 4: yield loc
+def peel_layer(locs, possibles):
+    peeled = []
+    adjacents = set()
+
+    for loc in possibles:
+        around = [z for z in grids.neighbors8(loc) if z in locs]
+        if len(around) < 4:
+            peeled.append(loc)
+            adjacents.update(around)
+
+    adjacents.difference_update(peeled)
+    return (peeled, adjacents)
 
 if __name__ == "__main__": util.run_solution(solution)
