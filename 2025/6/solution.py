f63701035316dafa9116bb334f170233dc2879e9
Sumudu Fernando <sumuduf@gmail.com>
2025-12-06 21:52:31 -0800

    [2025] Solution for day 6
    Straightforward problem which is mainly a parsing task. Got a bit
    tripped up because I forgot that my template strips trailing whitespace
    from each line (but easy to handle once I remembered, and anyway
    probably better to not assume the input has those spaces there).
    
    Sadly "grids" library was not particularly useful here (grid_getter
    *can* handle filling in spaces but is not that nice to use outside of
    gnumerate). However, the types of things I'd want (e.g. rotating,
    partitioning) are probably not worth implementing there vs. just
    learning how to use numpy for that stuff.

---
+++ 2025/6/solution.py
@@ -0,0 +1,42 @@
+from aoclib import util
+from math import prod
+
+def solution(lines):
+    digits, ops = lines[:-1], lines[-1]
+
+    # note: input is right-trimmed but for ease of coding we want to pad each
+    # line with spaces
+    n = len(digits)
+    m = max(len(row) for row in digits)
+    for (i, row) in enumerate(digits):
+        digits[i] = row + (" " * (m - len(row)))
+
+    # use the operators as convenient markers
+    cols = [i for (i, ch) in enumerate(ops) if ch != " "]
+    cols.append(m+1) # sentinel to bound the last set
+
+    part1_ans = 0
+    part2_ans = 0
+    for (i, j) in zip(cols, cols[1:]):
+        # use i:(j-1) to leave out the column of spaces
+        p1_nums = nums_horiz(digits, i, j-1)
+        p2_nums = nums_vert(digits, i, j-1)
+        match ops[i]:
+            case "*":
+                part1_ans += prod(p1_nums)
+                part2_ans += prod(p2_nums)
+            case "+":
+                part1_ans += sum(p1_nums)
+                part2_ans += sum(p2_nums)
+
+    return (part1_ans, part2_ans)
+
+def nums_horiz(digits, start, end):
+    for row in digits:
+        yield int(row[start:end])
+
+def nums_vert(digits, start, end):
+    for c in range(start, end):
+        yield int("".join(row[c] for row in digits))
+
+if __name__ == "__main__": util.run_solution(solution)
