9c5f444e47b99fbd2a2b029627b32a028f351915
Sumudu Fernando <sumuduf@gmail.com>
2025-12-08 22:57:19 -0800

    [2025] Solution for day 9
    Ended up using coordinate compression, but for whatever reason I forgot
    how to avoid typical mistakes with this kind of question (partially I
    confused myself alternating between thinking of grid squares vs. integer
    points). A sweep-based solution (without explicitly compressing)
    probably would have been quicker to get right.
    
    Conceptually the idea here is to look at rays slightly above/right of
    the integer points to avoid the corner (literally) cases.
    
    Originally I built a 2D boolean map of the compressed "world" and
    scanned that for rectangle "fullness", which wasted a lot of unnecessary
    time (both coding and runtime). Once I had the cross points for each row
    a simple check that there aren't any cross points between the left and
    right sides of the rectangle (and that we have an odd number before the
    first one) is all that is needed and also simpler to implement.
    
    Note: for my data set I found it was faster to switch x/y.

---
+++ 2025/9/solution.py
@@ -0,0 +1,59 @@
+from aoclib import util
+from aoclib.struct import Point2
+from bisect import bisect
+
+def solution(lines):
+    points = []
+    for line in lines:
+        x, y = map(int, line.split(","))
+        # x/y switched because it's faster for the shape of our data
+        points.append(Point2(y, x))
+
+    # work in compressed coordinate grid
+    xcoords = sorted(set(p.x for p in points))
+    ycoords = sorted(set(p.y for p in points))
+    xlook = dict((x, i) for (i, x) in enumerate(xcoords))
+    ylook = dict((y, i) for (i, y) in enumerate(ycoords))
+
+    def compress(p): return Point2(xlook[p.x], ylook[p.y])
+
+    # find x-coordinates where "insideness" changes, in each row
+    # detail: we're conceptually testing points slightly above/right of the
+    # integer points, so:
+    # - we can ignore horizontal edges
+    # - we can ignore the max y-coordinate (of candidate rectangles too)
+    # - we count <= the x-coordinate of interest (bisect = bisect_right)
+    ny = len(ycoords)
+    cross = [[] for _ in range(ny)]
+    prev = compress(points[-1])
+    for cp in map(compress, points):
+        if cp.x == prev.x:
+            for cy in range(*util.minmax(cp.y, prev.y)):
+                cross[cy].append(cp.x)
+        prev = cp
+    for cy in range(ny):
+        cross[cy] = sorted(set(cross[cy]))
+
+    part1_ans = 0
+    part2_ans = 0
+    for (i, p) in enumerate(points):
+        for q in points[i+1:]:
+            pq = p - q
+            area = (abs(pq.x) + 1) * (abs(pq.y) + 1)
+
+            part1_ans = max(part1_ans, area)
+            if area > part2_ans and is_full(cross, compress(p), compress(q)):
+                part2_ans = area
+
+    return (part1_ans, part2_ans)
+
+def is_full(cross, p, q):
+    # for each y-coordinate, [lx, rx) must be within one "inside" segment
+    lx, rx = util.minmax(p.x, q.x)
+    for cy in range(*util.minmax(p.y, q.y)):
+        k = bisect(cross[cy], lx)
+        if not (k & 1) or cross[cy][k] < rx:
+            return False
+    return True
+
+if __name__ == "__main__": util.run_solution(solution)
